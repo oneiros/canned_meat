af47b921a585e1acc4caa865bd96f7b94cac9653
David Roetzel <david@roetzel.de>
2015-05-28 13:53:17 +0200

    Remove foreign keys.
    Foreign keys do not seem to work (out of the box) with
    namespaced models right now.
    
    Since this is a new feature anyway and canned_meat should
    maybe support older rails versions at some point, I simply
    remove it here.

--- db/migrate/20150427142711_create_canned_meat_subscriptions.rb
+++ db/migrate/20150427142711_create_canned_meat_subscriptions.rb
@@ -2,7 +2,7 @@ class CreateCannedMeatSubscriptions < ActiveRecord::Migration
   def change
     create_table :canned_meat_subscriptions do |t|
       t.references :subscriber, polymorphic: true, index: {name: "index_canned_meat_subscriptions_on_subscriber_type_and_id"}
-      t.references :list, index: true, foreign_key: true
+      t.references :list, index: true
       t.datetime :unsubscribed_at
 
       t.timestamps null: false
--- db/migrate/20150428142736_create_canned_meat_campaigns.rb
+++ db/migrate/20150428142736_create_canned_meat_campaigns.rb
@@ -2,8 +2,8 @@ class CreateCannedMeatCampaigns < ActiveRecord::Migration
   def change
     create_table :canned_meat_campaigns do |t|
       t.string :name, index: {unique: true}
-      t.references :list, index: true, foreign_key: true
-      t.references :template, index: true, foreign_key: true
+      t.references :list, index: true
+      t.references :template, index: true
       t.string :status, null: false, default: 'draft'
       t.string :subject, null: false
       t.text :body
